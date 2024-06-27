2e31c800665c530ee8c2735fed34071e13bdf15a
Jackson <jacksonoliver2009@gmail.com>
2024-06-27 10:27:16 -0300

    conf final para deploy na vercel

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,10 @@ app.get('/leaderboard', (req, res) => {
     });
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
